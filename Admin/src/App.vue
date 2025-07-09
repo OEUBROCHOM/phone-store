cf90b6bc05a666dbccca0cb61135e9aabd9d725a
Oeub ROCHOM <oeub.rochom@student.passerellesnumeriques.org>
2025-07-09 20:41:07 +0700

    Initializ phone shop

---
+++ Admin/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-4 bg-blue-500 text-white rounded-lg w-1/5">
+    Hello admin
+  </div>
+</template>
---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-4 bg-blue-500 text-white rounded-lg w-1/5">
+    Hello Front
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
